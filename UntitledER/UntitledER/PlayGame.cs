9ae576d75100431f9dfd243603965f1a4bc2c5e8
Andrew Roska <andrew.l.roska@gmail.com>
2019-07-23 23:16:37 -0700

    Initialize

---
+++ UntitledER/UntitledER/PlayGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UntitledER
+{
+    public class PlayGame
+    {
+        public bool Title()
+        {
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                Console.Clear();
+                string title = @"
+    |\     /|( (    /|\__   __/\__   __/\__   __/( \      (  ____ \(  __  \ 
+    | )   ( ||  \  ( |   ) (      ) (      ) (   | (      | (    \/| (  \  )
+    | |   | ||   \ | |   | |      | |      | |   | |      | (__    | |   ) |
+    | |   | || (\ \) |   | |      | |      | |   | |      |  __)   | |   | |
+    | |   | || | \   |   | |      | |      | |   | |      | (      | |   ) |
+    | (___) || )  \  |   | |   ___) (___   | |   | (____/\| (____/\| (__/  )
+    (_______)|/    )_)   )_(   \_______/   )_(   (_______/(_______/(______/ 
+                                                                            
+        _______  _______              _______  _______  _______  _______ 
+       (  ____ \(  ____ )            (  ____ \(  ___  )(       )(  ____ \
+       | (    \/| (    )|            | (    \/| (   ) || () () || (    \/
+       | (__    | (____)|            | |      | (___) || || || || (__    
+       |  __)   |     __)            | | ____ |  ___  || |(_)| ||  __)   
+       | (      | (\ (               | | \_  )| (   ) || |   | || (      
+       | (____/\| ) \ \__            | (___) || )   ( || )   ( || (____/\
+       (_______/|/   \__/            (_______)|/     \||/     \|(_______/";
+                Console.WriteLine(title);
+                Console.WriteLine();
+                Console.Write("Press Space to play...");
+
+                keyInfo = Console.ReadKey();
+            }
+            while (keyInfo.Key != ConsoleKey.Spacebar);
+
+            return false;
+        }
+    }
+}
---
+++ UntitledER/UntitledER/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UntitledER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool play = true;
+            PlayGame game = new PlayGame();
+
+            while (play == true)
+            {
+                play = game.Title();
+                Console.Clear();
+            }
+
+            Console.WriteLine("Bye!  Thanks for playing!");
+            System.Threading.Thread.Sleep(1500);
+        }
+    }
+}
